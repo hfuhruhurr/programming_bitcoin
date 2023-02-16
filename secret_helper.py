e8173cda8dd788501191b67f215fc77b7305efc8
Doug Devine <dougdevine@gmail.com>
2023-02-15 21:25:55 -0500

    another night in the books

---
+++ secret_helper.py
@@ -0,0 +1,16 @@
+# this code isn't in the text (at least not in the part where it's first referenced)
+# it was discovered in the book's github repo
+
+from unittest import TestSuite, TextTestRunner
+
+import hashlib
+
+def run(test):
+	suite = TestSuite()
+	suite.addTest(test)
+	TextTestRunner().run(suite)
+
+
+def hash256(s):
+	'''two rounds of sha256'''
+	return hashlib.sha256(hashlib.sha256(s).digest()).digest()

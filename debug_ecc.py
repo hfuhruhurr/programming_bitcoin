2ea6a66af63d34cffc4e2d0cf388ecbe72c82bd0
Doug Devine <dougdevine@gmail.com>
2023-02-28 09:06:30 -0500

    pre-insane-debug snapshot

---
+++ debug_ecc.py
@@ -0,0 +1,5 @@
+# Imma figure out why Jimmy's code runs while my apparently identical code does not.
+# There are 11 methods used.
+# I will replace my code w/ Jimmy's until discovery.
+# Let's fucking go!
+
--- ecc.py
+++ ecc.py
@@ -9,17 +9,13 @@ class FieldElement:
 		self.num = num
 		self.prime = prime 
 
-
 	def __repr__(self):
-		# return f'FieldElement_{self.prime}({self.num})'
-		return f'{self.num}'
-
+		return f'FieldElement_{self.prime}({self.num})'
 
 	def __eq__(self, other):
 		if other is None:
 			return False
 		return self.num == other.num and self.prime == other.prime
-
 
 	def __ne__(self, other):
 		# my solution
@@ -30,14 +26,12 @@ class FieldElement:
 		# a better solution (since this should be the logical negation of ==)
 		return (not self == other)
 
-
 	def __add__(self, other):
 		if self.prime != other.prime:
 			error = f'Two different fields detected...no bueno.'
 			raise TypeError(error)
 		num = (self.num + other.num) % self.prime 
 		return self.__class__(num, self.prime)  # need to return a FieldElement class, not an integer
-
 
 	def __sub__(self, other):
 		if self.prime != other.prime:
@@ -46,7 +40,6 @@ class FieldElement:
 		num = (self.num - other.num) % self.prime 
 		return self.__class__(num, self.prime)  # need to return a FieldElement class, not an integer
 
-
 	def __mul__(self, other):
 		if self.prime != other.prime:
 			error = f'Two different fields detected...no bueno.'
@@ -54,35 +47,39 @@ class FieldElement:
 		num = (self.num * other.num) % self.prime
 		return self.__class__(num, self.prime)
 
-
-	def __rmul__(self, scalar):
-		num = (self.num * scalar) % self.prime 
-		return self.__class__(num, self.prime)
-	
-
-	def __truediv__(self, other):
-		if self.prime != other.prime:
-			error = f'Two different fields detected...no bueno.'
-			raise TypeError(error)
-		num = (self.num * other.num**(self.prime - 2)) % self.prime
-		return self.__class__(num, self.prime)
-
-
 	def __pow__(self, exponent):
 		n = exponent % (self.prime - 1)  # force exponent in [0..prime-2]
 		num = self.num**n % self.prime 
 		return self.__class__(num, self.prime)
 
+	def __truediv__(self, other):
+		if self.prime != other.prime:
+			error = f'Two different fields detected...no bueno.'
+			raise TypeError(error)
+		        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+		# num = (self.num * other.num**(self.prime - 2)) % self.prime  # POSSIBLE DOUG-INDUCED ERROR
+		num = (self.num * (other.num**(self.prime - 2) % self.prime)) % self.prime
+		return self.__class__(num, self.prime)
+
+	def __rmul__(self, scalar):
+		num = (self.num * scalar) % self.prime 
+		return self.__class__(num, self.prime)
+
 
 class Point:
 	def __init__(self, x, y, a, b):
-		self.a = a 
-		self.b = b  
 		self.x = x  
 		self.y = y 
+		self.a = a 
+		self.b = b  
 		if self.x is None and self.y is None:  # this is how we define the 'point at infinity', aka the additive identity
 			return  # won't pass the formula check below but it's a valid, albeit artificial, point on the curve
-		if self.y**2 != self.x**3 + a*x + b:
+		if self.y**2 != self.x**3 + a * x + b:
 			error = f'The point ({x}, {y}) is not on the curve, brah.'
 			raise ValueError(error)
 
@@ -100,37 +97,39 @@ class Point:
 			error = f'Point {self} is not on the same curve as Point {other}, chief.'
 			raise TypeError(error)
 
-		# Case 1: One of the points is the point at infinity
+		# Case 1: One of the points is the point at infinity.
 		if self.x is None:   # self is the point at infinity so added this to other results in other (additive identity, bro)
 			return other 
 
 		if other.x is None:  # ibid
 			return self 
 
-		# Case 2: The two points lie in a vertical line
+		# Case 2: The two points lie in a vertical line (same x) but are distinct (different y).
+		#         By definition, the sum is the point at infinity.
 		if self.x == other.x and self.y != other.y:
 			return self.__class__(None, None, self.a, self.b)  # remember to return a _class_, not just a number
 
-		# Case 3: the points are not in a vertical line but are different
-		if self.x != other.x:
-			slope = (other.y - self.y)/(other.x - self.x)
-			x3 = slope**2 - self.x - other.x
-			y3 = slope*(self.x - x3) - self.y
-			return self.__class__(x3, y3, self.a, self.b)
-		
-		# Case 4: the points are the same
+		# Case 3: The two points lie in a vertical line (same x) but are not distinct.
+		#         This can only happen when y = 0 --> vertical line tangent to curve
+		#         NB: Instead of figuring out what 0 is for each type, just use 0 * self.x.
 		if self == other and self.y == 0 * self.x:
-		# if self == other and self.y == 0:
 			return self.__class__(None, None, self.a, self.b)
+				
+		# Case 4: The two points do not lie in a vertical line (different x).
+		#         In this case, we need to find where the line connecting the two points intersects the curve.
+		if self.x != other.x:
+			slope = (other.y - self.y) / (other.x - self.x)
+			x = slope**2 - self.x - other.x
+			y = slope * (self.x - x) - self.y
+			return self.__class__(x, y, self.a, self.b)
+				
+		# Case 5: The two points are the same.
+		#         In this case, we need to find where the tangent line intersects the curve.
 		if self == other:
-			# print(f'type(self) = {type(self)}')
-			# print(f'type(self.y) = {type(self.y)}')
-			# if self.y == FieldElement(0, self.prime):  # the sub-case where the tangent line is vertical --> slope is undefined and will choke trying to divide by 0
-			# 	return self.__class__(None, None, self.a, self.b)  # by def'n, the point at infinity is the sum
 			slope = (3 * self.x**2 + self.a)/(2 * self.y)
-			x3 = slope**2 - 2 * self.x
-			y3 = slope*(self.x - x3) - self.y
-			return self.__class__(x3, y3, self.a, self.b)
+			x = slope**2 - 2 * self.x
+			y = slope * (self.x - x) - self.y
+			return self.__class__(x, y, self.a, self.b)
 
 
 	def __rmul__(self, scalar):
@@ -146,7 +145,12 @@ class Point:
 
 
 	def __repr__(self):
-		return f'The point is ({self.x}, {self.y})_{self.a}_{self.b}'
+		if self.x is None:
+			return 'Point(infinity)'
+		elif isinstance(self.x, FieldElement):
+			return f'Point({self.x.num},{self.y.num})_{self.a.num}_{self.b.num} FieldElement({self.x.prime})'
+		else:
+			return f'Point({self.x},{self.y})_{self.a}_{self.b})'
 
 
 class ECCTest(TestCase):

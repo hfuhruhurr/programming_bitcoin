801fdeb836e389a6fb54380537da607efc288d6d
Doug Devine <dougdevine@gmail.com>
2023-03-17 11:45:08 -0400

    thru Chapter 3

--- ecc.py
+++ ecc.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+from random import randint  ## NB: DO NOT USE FOR PRODUCTION...NOT RANDOM ENOUGH!!!
+import hashlib 
+import hmac 
 
 class FieldElement:
 
@@ -224,7 +227,63 @@ class S256Point(Point):
 		coef = scalar % N 
 		return super().__rmul__(coef)
 	
+	def verify(self, sig_hash, sig):
+		s_inv = pow(sig.s, N -2, N)  # curtesy Fermat's Little Theorem
+		u = sig_hash * s_inv % N
+		v = sig.r * s_inv % N 
+		expected_r = u * G + v * self 
+		return expected_r == sig.r 
+	
 
 # now that S256Point() is defined, we can add one more known constant, the generator point
 G = S256Point(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
 
+
+class Signature:
+
+	def __init__(self, r, s):
+		self.r = r
+		self.s = s 
+
+	def __repr__(self):
+		return f'Signature({self.r},{self.s})'
+	
+
+class PrivateKey:
+
+	def __init__(self, secret):
+		self.secret = secret
+		self.point = secret * G  # this is the public key, introduced here for convenience
+
+	def hex(self):
+		return f'{self.secret:x}'.zfill(64)
+	
+	def sign(self, z):
+		# k = randint(0, N)  # not to be trusted in production
+		k = self.deterministic_k(z)  # accepted way to get a unique k for each signature 
+		r = (k * G).x.num 
+		k_inv = pow(k, N - 2, N)
+		s = (z + r * self.secret) * k_inv % N 
+		if s > N/2:
+			s = N - s 
+		return Signature(r, s) 
+	
+	def deterministic_k(self, z):
+		k = b'\x00' * 32
+		v = b'\x01' * 32 
+		if z > N:
+			z -= N 
+		z_bytes = z.to_bytes(32, 'big')
+		secret_bytes = self.secret.to_bytes(32, 'big')
+		s256 = hashlib.sha256 
+		k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+		v = hmac.new(k, v, s256).digest()
+		k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+		v = hmac.new(k, v, s256).digest()
+		while True:
+			v = hmac.new(k, v, s256).digest()
+			candidate = int.from_bytes(v, 'big')
+			if candidate >= 1 and candidate < N:
+				return candidate
+			k = hmac.new(k, v + b'\x00', s256).digest()
+			v = hmac.new(k, v, s256).digest()
